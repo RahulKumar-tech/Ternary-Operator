bf9277917e9b759d2ab2713a9483905a6f3077bd
RAHUL KUMAR <77164649+RahulKumar-tech@users.noreply.github.com>
2024-03-30 20:04:15 +0530

    Add files via upload

---
+++ marks.cpp
@@ -0,0 +1,10 @@
+//Q) Given the marks of the student. If the marks are greater than 33 print the result as pass otherwise fail without using if-else statement.
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter marks : ";
+    cin>>n;
+    // condition ? true : false
+    (n>33) ? cout<<"pass" : cout<<"fail";
+    }
---
+++ ternaryOperator.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    // if(n%2==0){
+    //     cout<<"even";
+    // }
+    // else{
+    //     cout<<"odd";
+    // }
+
+    // it can be also writeen as because of single line statement
+    // if(n%2==0) cout<<"even";
+    // else cout<<"odd";
+    
+    //condition ? true : false
+    // means condition agar true agar false
+      (n%2==0) ? cout<<"even" : cout<<"odd";
+   }
